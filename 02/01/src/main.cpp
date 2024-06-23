35b2462bcd2fafb339a8c87ddadcc9c45ceccfcc
alex <alex@webgoal.ru>
2024-06-23 18:02:12 +0300

    Update

--- 02/01/src/main.cpp
+++ 02/01/src/main.cpp
@@ -1,7 +1,86 @@
+#include <string>
 #include <iostream>
-#include "paragraph.h"
-#include "reversed.h"
-#include "link.h"
+#include <algorithm>
+
+class Text
+{
+public:
+    virtual void render(const std::string &data) const
+    {
+        std::cout << data;
+    }
+    virtual void render(const std::string &link, const std::string &data)
+    {
+        std::cout << data;
+    }
+};
+
+class DecoratedText : public Text
+{
+public:
+    explicit DecoratedText(Text *text) : text_(text) {}
+    Text *text_;
+};
+
+class Paragraph : public DecoratedText
+{
+public:
+    explicit Paragraph(Text *text) : DecoratedText(text) {}
+    void render(const std::string &data) const
+    {
+        std::cout << "<p>";
+        text_->render(data);
+        std::cout << "</p>";
+    }
+};
+
+class Link : public DecoratedText
+{
+public:
+    explicit Link(Text *text) : DecoratedText(text) {}
+    void render(const std::string &link, const std::string &text) const
+    {
+        std::cout << "<a href=" << link << ">";
+        text_->render(link, text);
+        std::cout << "</a>";
+    }
+};
+
+class Reversed : public DecoratedText
+{
+public:
+    explicit Reversed(Text *text) : DecoratedText(text) {}
+    void render(const std::string &data) const
+    {
+        std::string copy(data);
+        std::reverse(copy.begin(), copy.end());
+        std::cout << copy;
+    }
+};
+
+class ItalicText : public DecoratedText
+{
+public:
+    explicit ItalicText(Text *text) : DecoratedText(text) {}
+    void render(const std::string &data) const
+    {
+        std::cout << "<i>";
+        text_->render(data);
+        std::cout << "</i>";
+    }
+};
+
+class BoldText : public DecoratedText
+{
+public:
+    explicit BoldText(Text *text) : DecoratedText(text) {}
+    void render(const std::string &data) const
+    {
+        std::cout << "<b>";
+        text_->render(data);
+        std::cout << "</b>";
+    }
+};
 
 int main()
 {
